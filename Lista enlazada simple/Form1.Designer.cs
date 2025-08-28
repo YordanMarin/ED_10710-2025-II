48bb74b69785efdda8e27a417d608874f23976e6
yordan <yord@gmail.com>
2025-08-28 12:07:35 -0500

    lista simple completo

--- Lista enlazada simple/Form1.Designer.cs
+++ Lista enlazada simple/Form1.Designer.cs
@@ -34,15 +34,18 @@
             this.btnInsertar = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.listDatos = new System.Windows.Forms.ListBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnOrdenar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(86, 20);
+            this.label1.Location = new System.Drawing.Point(64, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(460, 39);
+            this.label1.Size = new System.Drawing.Size(368, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "LISTA ENLAZADA SIMPLE";
             // 
@@ -50,61 +53,95 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(49, 102);
+            this.label2.Location = new System.Drawing.Point(37, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(154, 25);
+            this.label2.Size = new System.Drawing.Size(125, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Ingrese número:";
             // 
             // textNumero
             // 
             this.textNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textNumero.Location = new System.Drawing.Point(209, 97);
+            this.textNumero.Location = new System.Drawing.Point(157, 79);
+            this.textNumero.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textNumero.Name = "textNumero";
-            this.textNumero.Size = new System.Drawing.Size(347, 30);
+            this.textNumero.Size = new System.Drawing.Size(261, 26);
             this.textNumero.TabIndex = 2;
             // 
             // btnInsertar
             // 
             this.btnInsertar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnInsertar.Location = new System.Drawing.Point(42, 171);
+            this.btnInsertar.Location = new System.Drawing.Point(32, 139);
+            this.btnInsertar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnInsertar.Name = "btnInsertar";
-            this.btnInsertar.Size = new System.Drawing.Size(161, 47);
+            this.btnInsertar.Size = new System.Drawing.Size(121, 38);
             this.btnInsertar.TabIndex = 3;
             this.btnInsertar.Text = "Insertar";
             this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
             // 
             // btnEliminar
             // 
             this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminar.Location = new System.Drawing.Point(42, 236);
+            this.btnEliminar.Location = new System.Drawing.Point(32, 192);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(161, 47);
+            this.btnEliminar.Size = new System.Drawing.Size(121, 38);
             this.btnEliminar.TabIndex = 4;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // listDatos
             // 
             this.listDatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listDatos.FormattingEnabled = true;
-            this.listDatos.ItemHeight = 25;
-            this.listDatos.Location = new System.Drawing.Point(225, 161);
+            this.listDatos.ItemHeight = 20;
+            this.listDatos.Location = new System.Drawing.Point(169, 131);
+            this.listDatos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listDatos.Name = "listDatos";
-            this.listDatos.Size = new System.Drawing.Size(321, 379);
+            this.listDatos.Size = new System.Drawing.Size(242, 304);
             this.listDatos.TabIndex = 5;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(32, 243);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(121, 38);
+            this.btnBuscar.TabIndex = 6;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrdenar.Location = new System.Drawing.Point(32, 296);
+            this.btnOrdenar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(121, 38);
+            this.btnOrdenar.TabIndex = 7;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(687, 627);
+            this.ClientSize = new System.Drawing.Size(515, 509);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.listDatos);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnInsertar);
             this.Controls.Add(this.textNumero);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -120,6 +157,8 @@
         private System.Windows.Forms.Button btnInsertar;
         private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.ListBox listDatos;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnOrdenar;
     }
 }
 

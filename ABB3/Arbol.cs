8d096ba18a0721d0a743e02dce19ab259f36f7fe
hml <yordan@gmail.com>
2025-10-30 12:08:54 -0500

    sem11

---
+++ ABB3/Arbol.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ABB3
+{
+    internal class Arbol
+    {
+        public Nodo insertar(Nodo actual, int num)
+        {
+            Nodo nuevo = new Nodo(num);
+
+            if (actual == null) return nuevo;
+            if (num < actual.numero)
+                actual.izquierda = insertar(actual.izquierda, num);
+            else if (num > actual.numero)
+                actual.derecha = insertar(actual.derecha, num);
+            else MessageBox.Show("No duplicados");
+            return actual;
+        }
+
+        public void mostrar(Nodo actual, TreeView t, TreeNode tallo)
+        {
+            if (actual != null)
+            {
+                TreeNode raiz = new TreeNode(actual.numero.ToString());
+                if (tallo == null) t.Nodes.Add(raiz);
+                else tallo.Nodes.Add(raiz);
+
+                mostrar(actual.izquierda, t, raiz);
+                mostrar(actual.derecha, t, raiz);
+            }
+        }
+
+        public int minimo(Nodo actual)
+        {
+            while(actual.izquierda != null)
+                actual = actual.izquierda;
+            return actual.numero;
+        }
+
+        public int maximo(Nodo actual)
+        {
+            while (actual.derecha != null)
+                actual = actual.derecha;
+            return actual.numero;
+        }
+
+        public int SumaHojas(Nodo actual)
+        {
+            if (actual == null) return 0;
+            if(actual.izquierda ==null && actual.derecha ==null)
+                return actual.numero;
+
+            return SumaHojas(actual.izquierda) + SumaHojas(actual.derecha);
+        }
+
+        public int verificarBalance(Nodo actual)
+        {
+            if(actual == null) return 0;
+
+            int alturaDer = verificarBalance(actual.derecha);
+            int alturaIzq = verificarBalance(actual.izquierda);
+
+            if (alturaDer == -1) return -1; //no balanceado
+            if(alturaIzq == -1) return -1; //no balanceado
+
+            if(Math.Abs(alturaIzq - alturaDer) >1)
+                return -1; //desbalance encontrado - no balanceado
+
+            return Math.Max(alturaIzq, alturaDer) + 1;
+        }
+    }
+}
---
+++ ABB3/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ABB3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMinimo = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.textNumero = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnMaximo = new System.Windows.Forms.Button();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnBalanceo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMinimo
+            // 
+            this.btnMinimo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimo.Location = new System.Drawing.Point(49, 125);
+            this.btnMinimo.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMinimo.Name = "btnMinimo";
+            this.btnMinimo.Size = new System.Drawing.Size(97, 38);
+            this.btnMinimo.TabIndex = 31;
+            this.btnMinimo.Text = "Mínimo";
+            this.btnMinimo.UseVisualStyleBackColor = true;
+            this.btnMinimo.Click += new System.EventHandler(this.btnMinimo_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView1.Location = new System.Drawing.Point(183, 125);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(258, 281);
+            this.treeView1.TabIndex = 30;
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsertar.Location = new System.Drawing.Point(355, 71);
+            this.btnInsertar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(97, 38);
+            this.btnInsertar.TabIndex = 29;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // textNumero
+            // 
+            this.textNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNumero.Location = new System.Drawing.Point(183, 77);
+            this.textNumero.Margin = new System.Windows.Forms.Padding(2);
+            this.textNumero.Name = "textNumero";
+            this.textNumero.Size = new System.Drawing.Size(150, 26);
+            this.textNumero.TabIndex = 28;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 83);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 20);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Ingrese número:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(456, 31);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "ÁRBOL BINARIO DE BUSQUEDA";
+            // 
+            // btnMaximo
+            // 
+            this.btnMaximo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaximo.Location = new System.Drawing.Point(49, 178);
+            this.btnMaximo.Margin = new System.Windows.Forms.Padding(2);
+            this.btnMaximo.Name = "btnMaximo";
+            this.btnMaximo.Size = new System.Drawing.Size(97, 38);
+            this.btnMaximo.TabIndex = 32;
+            this.btnMaximo.Text = "Máximo";
+            this.btnMaximo.UseVisualStyleBackColor = true;
+            this.btnMaximo.Click += new System.EventHandler(this.btnMaximo_Click);
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuma.Location = new System.Drawing.Point(49, 233);
+            this.btnSuma.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(97, 38);
+            this.btnSuma.TabIndex = 33;
+            this.btnSuma.Text = "Suma";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // btnBalanceo
+            // 
+            this.btnBalanceo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBalanceo.Location = new System.Drawing.Point(49, 289);
+            this.btnBalanceo.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBalanceo.Name = "btnBalanceo";
+            this.btnBalanceo.Size = new System.Drawing.Size(97, 38);
+            this.btnBalanceo.TabIndex = 34;
+            this.btnBalanceo.Text = "Balanceo";
+            this.btnBalanceo.UseVisualStyleBackColor = true;
+            this.btnBalanceo.Click += new System.EventHandler(this.btnBalanceo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 450);
+            this.Controls.Add(this.btnBalanceo);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.btnMaximo);
+            this.Controls.Add(this.btnMinimo);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.textNumero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMinimo;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.TextBox textNumero;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnMaximo;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnBalanceo;
+    }
+}
+
